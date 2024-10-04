dca5d59b828f332d8dedcf82e6f9f427c7ad0c3d
anasyusuf <anas.yusuf200112@gmail.com>
2024-10-04 11:18:12 +0300

    Refactor App component to use react-query for data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Route";
 import { SidebarProvider } from "./SidebarContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <SidebarProvider>
-      <RouterProvider router={router} />
-    </SidebarProvider>
+    <QueryClientProvider client={queryClient}>
+      <SidebarProvider>
+        <RouterProvider router={router} />
+      </SidebarProvider>
+    </QueryClientProvider>
   );
 };
 
