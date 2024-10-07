c10f90a32f7418caca2ae543db3c253506fbad2d
anasyusuf <anas.yusuf200112@gmail.com>
2024-10-07 11:03:42 +0300

    Refactor App component to include Redux store for state management

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Route";
 import { SidebarProvider } from "./SidebarContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 // Create a client
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SidebarProvider>
-        <RouterProvider router={router} />
-      </SidebarProvider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <SidebarProvider>
+          <RouterProvider router={router} />
+        </SidebarProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 };
 
